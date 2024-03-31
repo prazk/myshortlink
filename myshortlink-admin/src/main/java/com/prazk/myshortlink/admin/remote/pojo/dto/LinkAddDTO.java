7020a28d741d81f08144839f9d718b5e1e943823
张晶鑫 <691854338@qq.com>
2024-03-31 23:07:09 +0800

    中台：修改短链接

--- myshortlink-admin/src/main/java/com/prazk/myshortlink/admin/remote/pojo/dto/LinkAddDTO.java
+++ myshortlink-admin/src/main/java/com/prazk/myshortlink/admin/remote/pojo/dto/LinkAddDTO.java
@@ -1,5 +1,6 @@
 package com.prazk.myshortlink.admin.remote.pojo.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ public class LinkAddDTO {
     //有效期类型 0：永久有效 1：用户自定义
     private Integer validDateType;
     //有效期
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime validDate;
     //描述
     private String description;
