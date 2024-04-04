8da392d36aa294d4881300e00a7c6c61e3ba3328
张晶鑫 <691854338@qq.com>
2024-04-04 21:59:09 +0800

    短链接移至回收站：移至回收站的同时删除缓存中的对应key

--- myshortlink-project/src/main/java/com/prazk/myshortlink/project/service/impl/RecycleBinServiceImpl.java
+++ myshortlink-project/src/main/java/com/prazk/myshortlink/project/service/impl/RecycleBinServiceImpl.java
@@ -2,11 +2,13 @@ package com.prazk.myshortlink.project.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.prazk.myshortlink.project.common.constant.CommonConstant;
+import com.prazk.myshortlink.project.common.constant.RedisConstant;
 import com.prazk.myshortlink.project.mapper.LinkMapper;
 import com.prazk.myshortlink.project.pojo.dto.RecycleAddDTO;
 import com.prazk.myshortlink.project.pojo.entity.Link;
 import com.prazk.myshortlink.project.service.RecycleBinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,17 +16,23 @@ import org.springframework.stereotype.Service;
 public class RecycleBinServiceImpl implements RecycleBinService {
 
     private final LinkMapper linkMapper;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @Override
     public void add(RecycleAddDTO recycleAddDTO) {
+        String shortUri = recycleAddDTO.getShortUri();
         // 根据【gid】和【shortUri】更新短链接状态
         LambdaUpdateWrapper<Link> wrapper = new LambdaUpdateWrapper<>();
         wrapper.eq(Link::getGid, recycleAddDTO.getGid())
-                .eq(Link::getShortUri, recycleAddDTO.getShortUri())
+                .eq(Link::getShortUri, shortUri)
                 .eq(Link::getDelFlag, CommonConstant.NOT_DELETED)
                 .eq(Link::getEnableStatus, CommonConstant.HAS_ENABLED);
 
         Link link = Link.builder().enableStatus(CommonConstant.NOT_ENABLED).build();
         linkMapper.update(link, wrapper);
+
+        // 删除缓存中的内容
+        String key = RedisConstant.GOTO_SHORT_LINK_KEY_PREFIX + shortUri;
+        stringRedisTemplate.delete(key);
     }
 }
