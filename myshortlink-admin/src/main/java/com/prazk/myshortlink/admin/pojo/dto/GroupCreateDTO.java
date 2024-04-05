45684e54f51062607f6c353764da18b43c223133
张晶鑫 <691854338@qq.com>
2024-04-05 14:37:00 +0800

    修复bug：GroupCreateDTO没有无参构造器

--- myshortlink-admin/src/main/java/com/prazk/myshortlink/admin/pojo/dto/GroupCreateDTO.java
+++ myshortlink-admin/src/main/java/com/prazk/myshortlink/admin/pojo/dto/GroupCreateDTO.java
@@ -2,12 +2,14 @@ package com.prazk.myshortlink.admin.pojo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 分组创建DTO
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GroupCreateDTO {
     private String name;
 }
