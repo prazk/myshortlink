50d186bcc3a9a98731fb32dd0dabf8cf352fdd7d
张晶鑫 <691854338@qq.com>
2024-04-16 13:32:23 +0800

    前后端联调： 1. 修改LinkStatsLogsPageDTO

--- myshortlink-project/project-pojo/src/main/java/com/prazk/myshortlink/project/pojo/dto/LinkStatsLogsPageDTO.java
+++ myshortlink-project/project-pojo/src/main/java/com/prazk/myshortlink/project/pojo/dto/LinkStatsLogsPageDTO.java
@@ -1,17 +1,14 @@
 package com.prazk.myshortlink.project.pojo.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-
-import java.time.LocalDate;
 
 @Data
 public class LinkStatsLogsPageDTO {
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate startDate;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate endDate;
+    private String startDate;
+    private String endDate;
     private String shortUri;
+    private String fullShortUrl;
+    private String gid;
     private Integer current;
     private Integer size;
 }
